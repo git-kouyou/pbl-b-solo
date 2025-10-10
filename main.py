6a513be635c013d46892e042fafc486dc2e9517d
Koyo Uemura <98743479+git-kouyou@users.noreply.github.com>
2025-10-10 15:41:52 +0900

    微編集

--- main.py
+++ main.py
@@ -13,10 +13,16 @@
 import random
 import typing
 
-
 # info is called when you create your Battlesnake on play.battlesnake.com
 # and controls your Battlesnake's appearance
 # TIP: If you open your Battlesnake URL in a browser you should see this data
+board_width = 0
+board_height = 0
+wall1 = []
+wall2 = []
+wall3 = []
+wall4 = []
+
 def info() -> typing.Dict:
     print("INFO")
 
@@ -32,6 +38,13 @@ def info() -> typing.Dict:
 # start is called when your Battlesnake begins a game
 def start(game_state: typing.Dict):
     print("GAME START")
+    global board_height, board_width, wall1, wall2, wall3, wall4
+    board_width = game_state['board']['width']
+    board_height = game_state['board']['height']
+    wall1 = [(x, -1) for x in range(-1, board_width + 1)]
+    wall2 = [(x, board_height) for x in range(-1, board_width + 1)]
+    wall3 = [(-1, y) for y in range(board_height)]
+    wall4 = [(board_width, y) for y in range(board_height)]
 
 
 # end is called when your Battlesnake finishes a game
@@ -43,68 +56,77 @@ def end(game_state: typing.Dict):
 # Valid moves are "up", "down", "left", or "right"
 # See https://docs.battlesnake.com/api/example-move for available data
 def move(game_state: typing.Dict) -> typing.Dict:
-
-    is_move_safe = {"up": True, "down": True, "left": True, "right": True}
-
     # We've included code to prevent your Battlesnake from moving backwards
+    global board_width, board_height, wall1, wall2, wall3, wall4
+    is_move_safe = {"up": True, "down": True, "left": True, "right": True}
+    checkmate = {"up": True, "down": True, "left": True, "right": True}
     my_head = game_state["you"]["body"][0]  # Coordinates of your head
     my_neck = game_state["you"]["body"][1]  # Coordinates of your "neck"
-
-    if my_neck["x"] < my_head["x"]:  # Neck is left of head, don't move left
-        is_move_safe["left"] = False
-
-    elif my_neck["x"] > my_head["x"]:  # Neck is right of head, don't move right
-        is_move_safe["right"] = False
-
-    elif my_neck["y"] < my_head["y"]:  # Neck is below head, don't move down
-        is_move_safe["down"] = False
-
-    elif my_neck["y"] > my_head["y"]:  # Neck is above head, don't move up
-        is_move_safe["up"] = False
+    impossible = [(body["x"], body["y"]) for body in game_state["you"]["body"]]
+    impossible += wall1 + wall2 + wall3 + wall4
 
     # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-    board_width = game_state['board']['width']
-    board_height = game_state['board']['height']
 
-    if my_head["x"] <= 0:
+    # if my_head["x"] <= 0:
+    #     is_move_safe["left"] = False
+
+    # if my_head["x"] >= board_width - 1:
+    #     is_move_safe["right"] = False
+
+    # if my_head["y"] <= 0:
+    #     is_move_safe["down"] = False
+
+    # if my_head["y"] >= board_height - 1:
+    #     is_move_safe["up"] = False
+
+    #壁と自分の体にぶつからないようにする
+    if (my_head["x"] - 1, my_head["y"]) in impossible:
         is_move_safe["left"] = False
 
-    if my_head["x"] >= board_width - 1:
+    if (my_head["x"] + 1, my_head["y"]) in impossible:
         is_move_safe["right"] = False
 
-    if my_head["y"] <= 0:
+    if (my_head["x"], my_head["y"] - 1) in impossible:
         is_move_safe["down"] = False
 
-    if my_head["y"] >= board_height - 1:
+    if (my_head["x"], my_head["y"] + 1) in impossible:
         is_move_safe["up"] = False
 
-    bodies = [(body["x"], body["y"]) for body in game_state["you"]["body"]]
+    #詰みへ入らないようにする?
+    if (my_head["x"] - 1, my_head["y"] + 1) in impossible and (my_head["x"] - 1, my_head["y"] - 1) in impossible:
+        checkmate["left"] = False
+        print("左無理")
 
-    # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-    if (my_head["x"] - 1, my_head["y"]) in bodies:
-        is_move_safe["left"] = False
+    if (my_head["x"] + 1, my_head["y"] + 1) in impossible and (my_head["x"] + 1, my_head["y"] - 1) in impossible:
+        checkmate["right"] = False
+        print("右無理")
 
-    if (my_head["x"] + 1, my_head["y"]) in bodies:
-        is_move_safe["right"] = False
+    if (my_head["x"] + 1, my_head["y"] + 1) in impossible and (my_head["x"] - 1, my_head["y"] + 1) in impossible:
+        checkmate["up"] = False
+        print("上無理")
 
-    if (my_head["x"], my_head["y"] - 1) in bodies:
-        is_move_safe["down"] = False
-
-    if (my_head["x"], my_head["y"] + 1) in bodies:
-        is_move_safe["up"] = False
+    if (my_head["x"] + 1, my_head["y"] - 1) in impossible and (my_head["x"] - 1, my_head["y"] - 1) in impossible:
+        checkmate["down"] = False
+        print("下無理")
 
     # Are there any safe moves left?
     safe_moves = []
-    for move, isSafe in is_move_safe.items():
-        if isSafe:
-            safe_moves.append(move)
-
-    if len(safe_moves) == 0:
-        print(f"MOVE {game_state['turn']}: No safe moves detected! Moving down")
-        return {"move": "down"}
+    if all(value == False for value in checkmate.values()):
+        print("多分詰みやけど進むわ")
+        for move, isSafe in is_move_safe.items():
+            if isSafe:
+                safe_moves.append(move)
+    else:
+        for move, isSafe in is_move_safe.items():
+            if isSafe and checkmate[move]:
+                safe_moves.append(move)
 
     # Choose a random move from the safe ones
-    next_move = random.choice(safe_moves)
+    if len(safe_moves) == 0:
+        print("No safe move detected! move down!")
+        next_move = "down"
+    else:
+        next_move = random.choice(safe_moves)
 
     # TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
     # food = game_state['board']['food']
@@ -112,7 +134,6 @@ def move(game_state: typing.Dict) -> typing.Dict:
     print(f"MOVE {game_state['turn']}: {next_move}")
     print(f"x:{my_head["x"]}, y:{my_head["y"]}")
     return {"move": next_move}
-
 
 # Start server when `python main.py` is run
 if __name__ == "__main__":
