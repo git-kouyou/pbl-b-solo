f4669880cd3e29bb7b979d2bbcb165015f1b32fe
@tibu <u311051g@ecs.osaka-u.ac.jp>
2025-10-10 16:09:28 +0900

    色

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ def info() -> typing.Dict:
 
     return {
         "apiversion": "1",
-        "author": "",  # TODO: Your Battlesnake Username
-        "color": "#888888",  # TODO: Choose color
+        "author": "6",  # TODO: Your Battlesnake Username
+        "color": "#FFC0CB",  # TODO: Choose color pink
         "head": "default",  # TODO: Choose head
         "tail": "default",  # TODO: Choose tail
     }
